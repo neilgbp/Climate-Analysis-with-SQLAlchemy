ff9508c1bcd15b2bb31373bc81e2de17de83e686
NeilP <neilgbpate1@gmail.com>
2019-02-23 11:56:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,30 @@
-import datetime as dt
 import numpy as np
-import pandas as pd
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine, func, inspect
 
 from flask import Flask, jsonify
+import datetime as dt
+
+
+# Relative Date
 
 
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///hawaii.sqlite")
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite",
+                       connect_args={'check_same_thread': False}, echo=True)
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
 
-# Save references to each table
+# Save reference to the table
 Measurement = Base.classes.measurement
 Station = Base.classes.station
-
 # Create our session (link) from Python to the DB
 session = Session(engine)
 
